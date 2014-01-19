456b96904af20d1953bea40bcf9fcafd00153e1f
houssem <houssem@PC-de-houssem>
2014-01-19 02:13:19 +0100

    Getting activities from server ..

--- src/com/audamob/doit/service/manager/ActivityServiceManager.java
+++ src/com/audamob/doit/service/manager/ActivityServiceManager.java
@@ -1,98 +1,147 @@
 package com.audamob.doit.service.manager;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
+import com.audamob.doit.model.DoItActivity;
+import com.audamob.doit.utils.ApplicationConstants;
+import com.audamob.doit.utils.JSONParser;
 
-public class ActivityServiceManager {
+public class ActivityServiceManager extends AsyncTask<String, String, String> {
 
-	public void addNewProfile(){
-		
-	}
-	
-	public void addNewActivity(){
-		
-	}
-	
-	public void getProfileInformation(){
-		
-	}
-	
-	public void getCategoriesByCreteria(){
-		
-	}
-	
-	public void getActivitiesByCreteria(){
-		
+	// Progress Dialog
+	private ProgressDialog pDialog;
+	private List<DoItActivity> activities;
+
+	public List<DoItActivity> getActivities() {
+		return activities;
 	}
 
-	public void getUsers(){
-		
+	public void setActivities(List<DoItActivity> activities) {
+		this.activities = activities;
 	}
-	
-//	public void sendRequest(String face_id, int face_won, int face_lost,
-//			String location,String name) {
-//		HttpClient httpclient = new DefaultHttpClient();
-//		HttpPost post_data = new HttpPost("http://audahub.fr/facemash/newVote.php");
-//
-//		try {
-//			List<NameValuePair> postValues = new ArrayList<NameValuePair>(1);
-//			postValues.add(new BasicNameValuePair("id", face_id));
-//			postValues.add(new BasicNameValuePair("won", "" + face_won));
-//			postValues.add(new BasicNameValuePair("lost", "" + face_lost));
-//			postValues.add(new BasicNameValuePair("location", location));
-//			postValues.add(new BasicNameValuePair("name", name));
-//			post_data.setEntity(new UrlEncodedFormEntity(postValues));
-//
-//			String line;
-//			HttpResponse response = httpclient.execute(post_data);
-//			InputStream is = response.getEntity().getContent();
-//			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-//			while ((line = rd.readLine()) != null) {
-//				Log.d("line : ", line);
-//			}
-//		} catch (ClientProtocolException e) {
-//			Log.d("line : ", e.toString());// host not found
-//		} catch (IOException e) {
-//			Log.d("line : ", e.toString());
-//		}
-//	}
-//	
-//	public void getInfos(String face_id) {
-//		HttpClient httpclient = new DefaultHttpClient();
-//		HttpPost post_data = new HttpPost(
-//				"http://audahub.fr/facemash/getinfos.php");
-//
-//		try {
-//			List<NameValuePair> postValues = new ArrayList<NameValuePair>(1);
-//			postValues.add(new BasicNameValuePair("id", face_id));
-//			post_data.setEntity(new UrlEncodedFormEntity(postValues));
-//
-//			String line;
-//			HttpResponse response = httpclient.execute(post_data);
-//			InputStream is = response.getEntity().getContent();
-//			BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-//			while ((line = rd.readLine()) != null) {
-//				Log.d("line : ", line);
-//			}
-//		} catch (ClientProtocolException e) {
-//			Log.d("line : ", e.toString());// host not found
-//		} catch (IOException e) {
-//			Log.d("line : ", e.toString());
-//		}
-//	}
+
+	// Creating JSON Parser object
+	JSONParser jsonParser = new JSONParser();
+
+	ArrayList<HashMap<String, String>> activitiesList;
+
+	// products JSONArray
+	JSONArray activityJSONArray = null;
+
+	// ALL JSON node names
+	private static final String TAG_ACTIVITY_SET = "activitySet";
+	private static final String TAG_ID = "id_activity";
+	private static final String TAG_NAME = "name";
+	private static final String TAG_PICTURE = "picture";
+	private static final String TAG_DESCRIPTION = "description";
+	private static final String TAG_CATEGORY = "category";
+	private static final String TAG_LOCATION = "location";
+	private static final String TAG_IMAGES = "images";
+	private static final String TAG_VIDEOS = "videos";
+	private static final String TAG_COMMENTS = "comments";
+	private static final String TAG_FOLLOWERS = "followers";
+	private static final String TAG_START_DATE = "start_date";
+	private static final String TAG_END_DATE = "end_date";
+
+	private Context currentContext;
+	private String fileName = "activity.json";
+
+	public ActivityServiceManager(Context currentContext, String fileName) {
+		this.currentContext = currentContext;
+		activities = new ArrayList<DoItActivity>();
+		this.fileName = fileName;
+
+	}
+
+	/**
+	 * Get Places from Json File
+	 */
+	public List<DoItActivity> getActivitiesFromJsonFile(String url) {
+
+		// Building Parameters
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+		// getting JSON string from URL
+		JSONObject json = jsonParser.makeHttpRequest(url, "GET", params);
+
+		// Check your log cat for JSON reponse
+		Log.d("All Activites JSON: ", json.toString());
+
+		try {
+			activityJSONArray = json.getJSONArray(TAG_ACTIVITY_SET);
+			// looping through All messages
+
+			for (int i = 0; i < activityJSONArray.length(); i++) {
+				JSONObject c = activityJSONArray.getJSONObject(i);
+
+				// Create activity Object
+				DoItActivity activity = new DoItActivity();
+				activity.setDoIdActivity(c.getString(TAG_ID));
+				activity.setDoDisplayName(c.getString(TAG_NAME));
+				activity.setDoCategoryName(c.getString(TAG_CATEGORY));
+				activity.setDoDescription(c.getString(TAG_DESCRIPTION));
+				activity.setDoPicture(c.getString(TAG_PICTURE));
+				activity.setDoNbreFollowers(c.getString(TAG_FOLLOWERS));
+				activity.setDoLocation(c.getString(TAG_LOCATION));
+				activity.setDoComments(c.getString(TAG_COMMENTS));
+				activity.setDoVideos(c.getString(TAG_VIDEOS));
+				activity.setDoImages(c.getString(TAG_IMAGES));
+				activity.setDoStartDate(c.getString(TAG_START_DATE));
+				activity.setDoEndDate(c.getString(TAG_END_DATE));
+
+				activities.add(activity);
+			}
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		Log.i("activities size", "" + activities.size());
+		return activities;
+
+		// End Get Activities from Json File
+	}
+
+	/**
+	 * Before starting background thread Show Progress Dialog
+	 * */
+	@Override
+	protected void onPreExecute() {
+		super.onPreExecute();
+		pDialog = new ProgressDialog(this.currentContext);
+		pDialog.setMessage("Loading activities ...\nPlease wait");
+		pDialog.setIndeterminate(false);
+		pDialog.setCancelable(false);
+		pDialog.show();
+	}
+
+	/**
+	 * getting Inbox JSON
+	 * */
+	protected String doInBackground(String... args) {
+
+		setActivities(getActivitiesFromJsonFile(ApplicationConstants.ACTIVITIES_JSON_URL
+				+ this.fileName));
+		return null;
+	}
+
+	/**
+	 * After completing background task Dismimport
+	 * fr.audasoft.facemash.activity.FaceMashActivity; import
+	 * fr.audasoft.facemash.util.JSONParser; import
+	 * fr.audasoft.faemash.model.Face; iss the progress dialog
+	 * **/
+	protected void onPostExecute(String file_url) {
+		// dismiss the dialog after getting all products
+		pDialog.dismiss();
+	}
 }
