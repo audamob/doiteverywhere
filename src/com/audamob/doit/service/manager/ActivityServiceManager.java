521713b3fedc6ddad265a1ba08090d76405c75fa
houssem <houssem@PC-de-houssem.home>
2014-02-12 23:17:03 +0100

    Json mock ...

--- src/com/audamob/doit/service/manager/ActivityServiceManager.java
+++ src/com/audamob/doit/service/manager/ActivityServiceManager.java
@@ -1,5 +1,8 @@
 package com.audamob.doit.service.manager;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -12,7 +15,6 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ListView;
@@ -58,6 +60,7 @@ public class ActivityServiceManager extends AsyncTask<String, String, String> {
 	private static final String TAG_FOLLOWERS = "followers";
 	private static final String TAG_START_DATE = "start_date";
 	private static final String TAG_END_DATE = "end_date";
+	private static final String PATH = "/doiteverywhere/json_mock";
 
 	private Activity currentContext;
 	private String fileName = "activity.json";
@@ -75,9 +78,86 @@ public class ActivityServiceManager extends AsyncTask<String, String, String> {
 	public ActivityServiceManager(Activity currentContext) {
 		this.currentContext = currentContext;
 	}
+	
+	public ActivityServiceManager(){
+		
+	}
 
 	/**
-	 * Get Places from Json File
+	 * Get Activities from Mock Json file ( TEMPORAIRE )
+	 */
+	public ArrayList<DoItActivity> getActivitiesFromMockFile(String fileName) {
+		String jsonContent = readFromFile(fileName);
+
+		try {
+
+			JSONObject json = new JSONObject(jsonContent);
+
+			// Check your log cat for JSON reponse
+			Log.d("All Activites JSON: ", json.toString());
+
+			activityJSONArray = json.getJSONArray(TAG_ACTIVITY_SET);
+			// looping through All messages
+
+			for (int i = 0; i < activityJSONArray.length(); i++) {
+				JSONObject c = activityJSONArray.getJSONObject(i);
+
+				// Create activity Object
+				DoItActivity activity = new DoItActivity();
+				activity.setDoIdActivity(c.getString(TAG_ID));
+				activity.setDoDisplayName(c.getString(TAG_NAME));
+				activity.setDoCategoryName(c.getString(TAG_CATEGORY));
+				activity.setDoDescription(c.getString(TAG_DESCRIPTION));
+				activity.setDoPicture(c.getString(TAG_PICTURE));
+				activity.setDoNbreFollowers(c.getString(TAG_FOLLOWERS));
+				activity.setDoLocation(c.getString(TAG_LOCATION));
+				activity.setDoComments(c.getString(TAG_COMMENTS));
+				activity.setDoVideos(c.getString(TAG_VIDEOS));
+				activity.setDoImages(c.getString(TAG_IMAGES));
+				activity.setDoStartDate(c.getString(TAG_START_DATE));
+				activity.setDoEndDate(c.getString(TAG_END_DATE));
+
+				activities.add(activity);
+			}
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		Log.i("activities size", "" + activities.size());
+		return activities;
+
+		// End Get Activities from Json File
+
+	}
+
+	/**
+	 * TO Read from the json Mock File
+	 * @param fileName
+	 * @return
+	 */
+	private String readFromFile(String fileName) {
+		
+		String fileContent = "";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(
+				PATH+fileName))) {
+
+			String sCurrentLine;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				fileContent = fileContent + sCurrentLine;
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return fileContent;
+	}
+
+	/**
+	 * Get Activities from Json File
 	 */
 	public ArrayList<DoItActivity> getActivitiesFromJsonFile(String url) {
 
@@ -176,8 +256,11 @@ public class ActivityServiceManager extends AsyncTask<String, String, String> {
 	 * */
 	protected String doInBackground(String... args) {
 
-		setActivities(getActivitiesFromJsonFile(ApplicationConstants.ACTIVITIES_JSON_URL
-				+ this.fileName));
+//		setActivities(getActivitiesFromJsonFile(ApplicationConstants.ACTIVITIES_JSON_URL
+//				+ this.fileName));
+		
+		//FIXME : TMP jusquau correction du problème de l'API de service
+		setActivities(getActivitiesFromMockFile("activity.json"));
 		return null;
 	}
 
@@ -195,5 +278,10 @@ public class ActivityServiceManager extends AsyncTask<String, String, String> {
 		Log.d("APIAPI", "s " + " " + this.doListDoItActivities);
 		this.doListDoItActivities.setAdapter(new DoItActivitiesListAdapter(
 				this.currentContext, getActivities()));
+	}
+	
+	public static void main(String[] args) {
+		ActivityServiceManager asm = new ActivityServiceManager();
+		System.out.println("content : "+asm.readFromFile("activity.json"));
 	}
 }
